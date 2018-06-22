f9099682d6c8876d9c0660882c04ba34ef7c48cf
qinfenzou <qinfenzou@gmail.com>
2018-06-22 14:12:42 +0800

    init vue program person

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 引用API文件
+import api from './api/index.js'
+// 将API方法绑定到全局
+Vue.prototype.$api = api
+
+// 引用API文件
+import utils from './utils/index.js'
+// 将API方法绑定到全局
+Vue.prototype.$utils = utils
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/page/content.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>conten page
+  <el-button type="primary" icon="el-icon-search">饿搜索</el-button>
+  </div>
+</template>
